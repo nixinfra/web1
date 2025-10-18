c2cfbfa2163f9d479024d75d959c0ba5035088e5
longbc <longdib86@gmail.com>
2025-10-18 02:23:20 +0000

    fix 1

---
+++ public/searchindex.en.js
@@ -0,0 +1,98 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "",
+    "content": "Welcome to SysNetHub – Your Hub for Practical, Essential Knowledge in Network and System Infrastructure.\nThe Importance of Network and System Infrastructure No System, No Service. No Network, No Connection.\nEvery click, every application, every AI model – all rely on a foundation that most people tend to overlook. System and Network infrastructure is not just a part of IT, it is the core that enables every other field to be possible. From the data center to the cloud platform, from AI reasoning to IoT devices – nothing can operate without a solid backbone. Here, we build, refine, and advance it with you.\nNo Basics, No Advance. No Basics, No Advance.\nEveryone is chasing the next great thing – AI, cloud computing, automation, whatever is coming next. But to stand firm and thrive in this ever-demanding era, you need to understand the fundamentals – the systems, the networking, the very platform everything is built upon. The deeper you understand the basics, the further you can advance. No solid foundation, no long-term progress.\nWebsite Ethos: Website Ethos:\nNo fanfare or elaborate prose. We get straight to the point, presenting content as concisely as possible to help users quickly learn, search, and apply knowledge.\nTarget Audience: Beginners and students needing to learn Network and System knowledge. Individuals seeking to refresh knowledge for relevant certification exams related to networking and systems. Working professionals who need to update knowledge or quickly look up information for application. How to Use This Website:\nAll content (continuously updated and expanding) is located on the left-hand menu. Choose the area you are interested in to begin exploring.\nWe wish you an effective and inspiring learning journey with SysNetHub.",
+    "description": "A comprehensive resource for Network, System, and Infrastructure studies.",
+    "tags": [],
+    "title": "HELLO",
+    "uri": "/index.html"
+  },
+  {
+    "breadcrumb": "HELLO \u003e LAB",
+    "content": "Hands-on Linux Labs.",
+    "description": "Hands-on Linux Labs.",
+    "tags": [],
+    "title": "Linux",
+    "uri": "/lab/linux/index.html"
+  },
+  {
+    "breadcrumb": "HELLO \u003e QUIZ",
+    "content": "Linux Quizzes.",
+    "description": "Linux Quizzes.",
+    "tags": [],
+    "title": "Linux",
+    "uri": "/quiz/linux/index.html"
+  },
+  {
+    "breadcrumb": "HELLO",
+    "content": "Test your knowledge here. Choose a topic from the sub-menus on the left.",
+    "description": "Test your knowledge here. Choose a topic from the sub-menus on the left.",
+    "tags": [],
+    "title": "QUIZ",
+    "uri": "/quiz/index.html"
+  },
+  {
+    "breadcrumb": "HELLO \u003e QUIZ",
+    "content": "Cisco Networking Quizzes.",
+    "description": "Cisco Networking Quizzes.",
+    "tags": [],
+    "title": "Cisco",
+    "uri": "/quiz/cisco/index.html"
+  },
+  {
+    "breadcrumb": "HELLO",
+    "content": "Practical labs and tutorials on various systems. Choose a system from the sub-menus on the left.",
+    "description": "Practical labs and tutorials on various systems. Choose a system from the sub-menus on the left.",
+    "tags": [],
+    "title": "LAB",
+    "uri": "/lab/index.html"
+  },
+  {
+    "breadcrumb": "HELLO \u003e LAB",
+    "content": "Windows Server and Desktop Labs.",
+    "description": "Windows Server and Desktop Labs.",
+    "tags": [],
+    "title": "Windows",
+    "uri": "/lab/windows/index.html"
+  },
+  {
+    "breadcrumb": "HELLO \u003e LAB",
+    "content": "Cisco Networking Labs.",
+    "description": "Cisco Networking Labs.",
+    "tags": [],
+    "title": "Cisco",
+    "uri": "/lab/cisco/index.html"
+  },
+  {
+    "breadcrumb": "HELLO",
+    "content": "Quick references and cheat sheets.",
+    "description": "Quick references and cheat sheets.",
+    "tags": [],
+    "title": "NOTES",
+    "uri": "/short/index.html"
+  },
+  {
+    "breadcrumb": "HELLO",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "breadcrumb": "HELLO",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "series",
+    "uri": "/series/index.html"
+  },
+  {
+    "breadcrumb": "HELLO",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  }
+]
